0ad8b333a7f4a599fdd45082be768c9a9fda559a
OrlandoBurgos34 <programador3423@gmail.com>
2024-09-09 22:30:26 -0500

    users set up

--- src/app/modules/creacion-usuarios/creacion-usuarios.component.ts
+++ src/app/modules/creacion-usuarios/creacion-usuarios.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-creacion-usuarios',
   templateUrl: './creacion-usuarios.component.html',
-  styleUrl: './creacion-usuarios.component.css'
+  styleUrl: './creacion-usuarios.component.css',
 })
-export class CreacionUsuariosComponent {
+export class CreacionUsuariosComponent implements OnInit {
+  userForm!: FormGroup;
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.userForm = this.fb.group({
+      name: ['', [Validators.required, Validators.maxLength(50)]],
+      lastName: ['', [Validators.required, Validators.maxLength(50)]],
+      dateOfBirth: [Date, [Validators.required]], //TODO: verificar
+      email: [
+        '',
+        [Validators.required, Validators.email, Validators.maxLength(50)],
+      ],
+      documentNumber: [
+        0,
+        [Validators.required, Validators.minLength(7), Validators.maxLength(7)],
+      ], //TODO: corregir
+      area: [0, [Validators.required, Validators.minLength(6)]], // TODO: corregir
+      salary: ['', [Validators.required, Validators.minLength(6)]], // TODO: corregir
+      status: ['', [Validators.required, Validators.minLength(6)]], // TODO: corregir
+    });
+  }
+
+  get emailErrorMessage(): string | null {
+    const emailControl = this.userForm.get('email');
+    if (emailControl?.hasError('required')) {
+      return 'El correo electrónico es obligatorio.';
+    }
+    if (emailControl?.hasError('email')) {
+      return 'El correo electrónico no es válido.';
+    }
+    return null;
+  }
+
+  get passwordErrorMessage(): string | null {
+    const passwordControl = this.userForm.get('password');
+    if (passwordControl?.hasError('required')) {
+      return 'La contraseña es obligatoria.';
+    }
+    if (passwordControl?.hasError('minlength')) {
+      return 'La contraseña debe tener al menos 6 caracteres.';
+    }
+    return null;
+  }
 }
