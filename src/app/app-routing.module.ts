4c4c0a6da8c78d7063e23209b9ea75cefc00df36
OrlandoBurgos34 <programador3423@gmail.com>
2024-09-09 22:04:33 -0500

    users first set up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutComponent } from './shared/layout/layout.component';
 import { DashboardComponent } from './modules/dashboard/dashboard.component';
+import { CreacionUsuariosComponent } from './modules/creacion-usuarios/creacion-usuarios.component';
 
 const routes: Routes = [
   {
     path: '',
     component: LayoutComponent,
     children: [
-      {path: '',component: DashboardComponent}
+      {path: '', component: DashboardComponent},
+      {path: 'users', component: CreacionUsuariosComponent}
     ],
   },
 ];
